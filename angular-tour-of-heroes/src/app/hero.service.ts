52d8d143a5234713c5df9a45d6a3b3957f9fb8d3
Saskia Riederer <30173909+Saskia-R@users.noreply.github.com>
2020-11-11 15:32:14 +0100

    added routing and reworked links in app with routes

--- angular-tour-of-heroes/src/app/hero.service.ts
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -18,4 +18,10 @@ export class HeroService {
     // returns an Observable, that emits the array of mock heros as single value
     return of(HEROES);
   }
+
+  getHero(id: number): Observable<Hero> {
+    // TODO: send the message _after_ fetching the hero
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find((hero) => hero.id === id));
+  }
 }
